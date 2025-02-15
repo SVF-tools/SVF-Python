9d80ba3c52dbe85ded93f960d78f9e3670a2f063
bjjwwang <bjjwwang@github.com>
2025-02-15 14:43:18 +1100

    fix build.sh

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ for version in "${PYTHON_VERSIONS[@]}"; do
     rm -rf build
     PYTHON_EXEC="python${version}"
     # if PYTHON_EXEC not exist
-    if [! -x "$(command -v $PYTHON_EXEC)" ]; then
+    if [ ! -x "$(command -v $PYTHON_EXEC)" ]; then
         echo "Python ${version} not found. Please install pyenv, and install the PYTHON_VERSIONS: ${version}. Then set it as global."
         continue
     fi
@@ -24,8 +24,12 @@ for version in "${PYTHON_VERSIONS[@]}"; do
 
     PYBIND11_DIR=$(${PYTHON_EXEC} -m pybind11 --cmakedir)
 
-    SVF_DIR=${SVF_DIR} LLVM_DIR=${LLVM_DIR} Z3_DIR=${Z3_DIR} PYBIND11_DIR=${PYBIND11_DIR} ${PYTHON_EXEC} setup.py bdist_wheel --plat-name macosx-11.0-arm64
-
+    # if mac arm , platfrom name is macosx-11.0-arm64, if linux x86 64, platfrom name is manylinux2014_x86_64
+    if [ "$(uname)" == "Darwin" ]; then
+        SVF_DIR=${SVF_DIR} LLVM_DIR=${LLVM_DIR} Z3_DIR=${Z3_DIR} PYBIND11_DIR=${PYBIND11_DIR} ${PYTHON_EXEC} setup.py bdist_wheel --plat-name macosx-11.0-arm64
+    else
+        SVF_DIR=${SVF_DIR} LLVM_DIR=${LLVM_DIR} Z3_DIR=${Z3_DIR} PYBIND11_DIR=${PYBIND11_DIR} ${PYTHON_EXEC} setup.py bdist_wheel --plat-name manylinux2014_x86_64
+    fi
     if [ $? -ne 0 ]; then
         echo "Building for Python ${version} failed."
     else
