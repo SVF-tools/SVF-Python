836b8e40d56f5f7cf7c12adc30d523d8af1c1688
bjjwwang <bjjwwang@github.com>
2025-02-15 11:15:33 +1100

    fix some compile err

--- setup.py
+++ setup.py
@@ -68,13 +68,16 @@ class CMakeBuild(build_ext):
             raise RuntimeError("Could not find built libpysvf.so")
 
         # Rename and move to expected location
-        # 获取当前 Python 版本的 .so 文件后缀（如 .cpython-38-x86_64-linux-gnu.so）
+        # get dylib suffix of current python version（e.g cpython-38-x86_64-linux-gnu.so）
         so_suffix = sysconfig.get_config_var("EXT_SUFFIX")
 
-        # 生成适配当前 Python 版本的目标路径
         so_target = os.path.join(self.build_lib, "pysvf", "pysvf" + so_suffix)
         os.makedirs(os.path.dirname(so_target), exist_ok=True)
         shutil.copyfile(built_so[0], so_target)
+        # if platform is darwin, we need to change the libz3.dylib path in pysvf.{$SO_SUFFIX}
+        # run  install_name_tool -change libz3.dylib @rpath/libz3.dylib pysvf.{$SO_SUFFIX}  to so_target
+        if platform.system() == "Darwin":
+            subprocess.run(["install_name_tool", "-change", "libz3.dylib", "@rpath/libz3.dylib", so_target], check=True)
 
         # if pysvf/SVF/Release-build exist, remove
         if os.path.exists(os.path.join(self.build_lib, "pysvf", "SVF")):
