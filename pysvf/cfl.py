217d4f023ded0d7ac81f510e2dca7452c23a8ba8
Jiawei Wang <jackwang834@gmail.com>
2025-02-28 15:24:12 +1100

    Update cfl.py

--- pysvf/cfl.py
+++ pysvf/cfl.py
@@ -1,12 +1,9 @@
 import sys
-print("[DEBUG] ae.py is executing!")
 
 from . import run_tool
 
 def main():
-    print("[DEBUG] Inside main() of cfl.py")
     run_tool("cfl", sys.argv[1:])
 
 if __name__ == "__main__":
-    print("[DEBUG] __name__ == '__main__', calling main()")
     main()
